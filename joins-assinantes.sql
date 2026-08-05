6758a237b05b1233d39a17a80a2d5057e8faa93c
Laura <laura.laurindo@escola.pr.gov.br>
2026-08-05 10:39:44 -0300

    Modify join type and order results in SQL query
    Changed RIGHT JOIN to LEFT JOIN and added ORDER BY clause.

--- joins-assinantes.sql
+++ joins-assinantes.sql
@@ -45,7 +45,7 @@ SELECT * FROM Telefone;
 
  SELECT a.nm_assinante, e.ds_endereco, t.n_fone FROM Assinante a INNER JOIN Endereco e ON a.cd_assinante = e.cd_assinante INNER JOIN Telefone t ON e.cd_endereco = t.cd_endereco;
 
- SELECT a.nm_assinante, r.ds_ramo FROM Assinante a RIGHT JOIN Ramo_Atividade r ON a.cd_ramo = r.cd_ramo;
+SELECT a.nm_assinante, r.ds_ramo FROM Assinante a LEFT JOIN Ramo_Atividade r ON a.cd_ramo = r.cd_ramo ORDER BY r.ds_ramo, a.nm_assinante;
 
  SELECT a.nm_assinante FROM Assinante a INNER JOIN Tipo_Assinante ta ON a.cd_tipo = ta.cd_tipo INNER JOIN Endereco e ON a.cd_assinante = e.cd_assinante INNER JOIN Municipio m ON e.cd_municipio = m.cd_municipio WHERE m.ds_municipio = 'Pelotas' AND ta.ds_tipo = 'Residencial';
 
